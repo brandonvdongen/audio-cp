7ca66e59442761e7840496d448f69178a63f8ce3
Egbert Verhage <egbert@eggiecode.org>
2017-06-21 22:28:47 +0200

    Added login function to AuthController

--- controllers/auth.class.php
+++ controllers/auth.class.php
@@ -7,11 +7,14 @@ class AuthController {
     private $mysqli = null;
 
     public function __construct($oMysqliConn) {
-        if($hMySQLiConn instanceof mysqli)
+        if($oMysqliConn)instanceof mysqli)
             throw new Exception("Private profide a mysqli object");
+        $this->mysqli = $oMysqliConn;
 
         if(isset($_SESSION[self::SESSION_VAR])) {
-            $this->verifyUser($_SESSION[self::SESSION_VAR]);
+            $bSuccess = $this->verifyUser($_SESSION[self::SESSION_VAR]);
+            if(!$bSuccess)
+                unset($_SESSION[self::SESSION_VAR]);
         }         
 
     }
@@ -19,15 +22,14 @@ class AuthController {
     public function verifyUser($iUserID) {
         // Check is userId is nummeric
         if(!is_numeric($iUserID)) {
-            unset($_SESSION[self::SESSION_VAR];
-            return null;
+            return false;
         } 
 
         //Query the database;
         
         $sQuery = "
             SELECT 
-              id_user as id, 
+              id_user AS id, 
               username,
               nickname
             FROM users 
@@ -35,16 +37,40 @@ class AuthController {
             ";
         $oResult = $this->mysqli->query($sQuery);
         if($oResult->num_rows != 1) {
-            unset($_SESSION[self::SESSION_VAR];
-            return;
+            return false;
         }
         
         $this->user = $oResult->fetch_object('User');
+        return true;
  
     } 
 
     public function getUser() {
         return $this->user;
+    }
+
+    public function login($username, $password) {
+
+        $sQuery = "
+            SELECT 
+                id_user,
+                password
+            FROM users 
+            WHERE username = '".$this->mysqli->real_escape_string($username)."'
+            ";
+        $oResult = $this->mysqli->query($sQuery);
+        if($oResult->num_rows != 1) {
+            return false;
+        }
+        
+        $aRow = $oResult->fetch_assoc();
+        $bSuccess = password_verify($password, $aRow['password']);
+
+        if(!$bSuccess)
+            return false;
+
+        $_SESSION[self::SESSION_VAR] = $aRow['id_user'];
+        return true;
     }
 
 }
